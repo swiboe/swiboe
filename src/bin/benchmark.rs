8b3b96b63c249842a750cc780800a647d30317b7
Holger Rapp <HolgerRapp@gmx.net>
2015-08-11 09:44:26 +0200

    Adds a trivial benchmark program.
    It just creates and deletes buffers all day.

---
+++ src/bin/benchmark.rs
@@ -0,0 +1,37 @@
+#![cfg(not(test))]
+
+extern crate serde;
+extern crate switchboard;
+extern crate tempdir;
+
+#[path="../../tests/support/mod.rs"] mod support;
+
+use serde::json;
+use support::{TestHarness};
+use switchboard::client::Client;
+use switchboard::ipc;
+use switchboard::plugin_buffer;
+
+fn main() {
+    let t = TestHarness::new();
+    let active_client = Client::connect(&t.socket_name);
+
+    loop {
+        let new_response: plugin_buffer::NewResponse = match active_client.call(
+            "buffer.new", &plugin_buffer::NewRequest {
+                content: Some("bli\nbla\nblub".into()),
+            }).wait().unwrap()
+        {
+            ipc::RpcResult::Ok(value) => json::from_value(value).unwrap(),
+            err => panic!("{:?}", err),
+        };
+
+        let _: plugin_buffer::DeleteResponse = match active_client.call(
+            "buffer.delete", &plugin_buffer::DeleteRequest {
+                buffer_index: new_response.buffer_index
+            }).wait().unwrap() {
+            ipc::RpcResult::Ok(value) => json::from_value(value).unwrap(),
+            err => panic!("{:?}", err),
+        };
+    }
+}
