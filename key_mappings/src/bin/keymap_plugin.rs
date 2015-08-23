cac28324f5019e43d92def4ebefa0e0c3bce0d63
Holger Rapp <HolgerRapp@gmx.net>
2015-08-23 16:17:32 +0200

    First thoughts on how keymaps might look.

--- key_mappings/src/bin/keymap_plugin.rs
+++ key_mappings/src/bin/keymap_plugin.rs
@@ -4,6 +4,7 @@ extern crate libc;
 extern crate lua;
 extern crate swiboe;
 
+use std::collections::{HashMap, HashSet};
 use std::path;
 use std::thread;
 use swiboe::client;
@@ -25,6 +26,21 @@ unsafe extern "C" fn tan(L: *mut lua_State) -> c_int {
 const MATHX_LIB: [(&'static str, Function); 1] = [
   ("tan", Some(tan)),
 ];
+
+type Key = String;
+enum KeyMap {
+    Coord(HashSet<Key>),
+    Arpeggio(Vec<Key>),
+}
+
+
+struct KeymapHandler {
+    keymaps: HashMap<String, String>,
+}
+
+struct KeymapPlugin {
+    buffers: Arc<RwLock<KeymapHandler>>,
+}
 
 fn main() {
     let matches = clap::App::new("term_gui")
@@ -49,6 +65,27 @@ fn main() {
     state.set_global("swiboe");
 
     state.do_file("test.lua");
+
+
+    let plugin = BufferPlugin {
+        buffers: Arc::new(RwLock::new(BuffersManager::new(client.clone()))),
+        client: client,
+    };
+
+    let new = Box::new(New { buffers: plugin.buffers.clone() });
+    plugin.client.new_rpc("buffer.new", new);
+
+    let delete = Box::new(Delete { buffers: plugin.buffers.clone() });
+    plugin.client.new_rpc("buffer.delete", delete);
+
+    let get_content = Box::new(GetContent { buffers: plugin.buffers.clone() });
+    plugin.client.new_rpc("buffer.get_content", get_content);
+
+    let open = Box::new(Open { buffers: plugin.buffers.clone() });
+    plugin.client.new_rpc("buffer.open", open);
+
+    let list = Box::new(List { buffers: plugin.buffers.clone() });
+    plugin.client.new_rpc("buffer.list", list);
 
 
     // NOCOM(#sirver): a client.spin_forever would be cool.
