7d617fbbeb2482ec087aeb24bd1b3b2517cfc108
Holger Rapp <HolgerRapp@gmx.net>
2015-09-04 08:36:26 +0200

    WIP - returning strings from callbacks.

--- c_client/src/lib.rs
+++ c_client/src/lib.rs
@@ -2,10 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE.txt
 // in the project root for license information.
 
+#![feature(cstr_memory)]
+
 extern crate libc;
 
 use libc::c_char;
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
 use std::str;
 
 #[no_mangle]
@@ -15,4 +17,15 @@ pub extern "C" fn hello(c_buf: *const libc::c_char) {
     let str_slice: &str = str::from_utf8(buf).unwrap();
     // let str_buf: String = str_slice.to_owned();  // if necessary
     println!("#sirver str_slice(): {:#?}", str_slice);
+}
+
+#[no_mangle]
+pub extern "C" fn hello1(cb: extern fn(i32, *mut *const libc::c_char)) {
+    println!("In rust!");
+    let mut ptr: *const libc::c_char;
+    let c_buf = cb(42, &mut ptr);
+    let c_str = unsafe { CStr::from_ptr(c_buf) };
+    let buf: &[u8] = c_str.to_bytes();
+    let str_slice: &str = str::from_utf8(buf).unwrap();
+    println!("Back in rust: {}", str_slice);
 }
--- c_client/test.py
+++ c_client/test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright (c) The Swiboe development team. All rights reserved.
 # Licensed under the Apache License, Version 2.0. See LICENSE.txt
@@ -9,3 +9,10 @@ sw = ctypes.cdll.LoadLibrary("target/debug/libswiboe.dylib")
 
 sw.hello(u"Löwe 老虎 Léopard".encode('utf-8'))
 sw.hello(u"ASCII żółć 🇨🇭 한".encode('utf-8'))
+
+def callback(arg, str_pointer):
+    str_pointer = "Hello again: %i" % arg
+
+
+CALLBACK = ctypes.CFUNCTYPE(None, ctypes.c_int32, ctypes.POINTER(ctypes.c_char_p))
+sw.hello1(CALLBACK(callback))
