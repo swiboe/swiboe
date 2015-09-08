cf74dc8b66e0349ac8f3064267134ff6b3aedec5
Holger Rapp <HolgerRapp@gmx.net>
2015-09-08 21:44:44 +0200

    Pull out testing into a shell script.

---
+++ travis_test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ex
+
+test_crate() {
+   DIR=$1; shift;
+   pushd $DIR
+
+   travis-cargo build
+   travis-cargo test
+   travis-cargo bench
+
+   popd
+}
+
+# Build documentation.
+pushd doc
+sphinx-build -b html -W source build/html
+popd
+
+test_crate "."
+test_crate "subsequence_match"
+test_crate "gui"
+test_crate "term_gui"
