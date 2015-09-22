1c4b54832c120a97070cb43685d69b2a6e28f4a1
Holger Rapp <HolgerRapp@gmx.net>
2015-09-22 08:45:41 +0200

    Added command line flags to the python testing.

--- c_client/swiboe.py
+++ c_client/swiboe.py
@@ -10,60 +10,87 @@ import json
 import os
 import platform
 import time
-
-# TODO(sirver): This needs extending for Windows.
-LIBRARY_NAME = os.getenv("SWIBOE_LIBRARY")
-if LIBRARY_NAME is None:
-    if platform.system() == "Darwin":
-        LIBRARY_NAME = "libswiboe.dylib"
-    else:
-        LIBRARY_NAME = "libswiboe.so"
-library = ctypes.cdll.LoadLibrary(LIBRARY_NAME)
+import sys
 
 PtrClient = c_void_p
-library.swiboe_connect.restype = PtrClient
-library.swiboe_connect.argtypes = [c_char_p]
-
-library.swiboe_disconnect.restype = None
-library.swiboe_disconnect.argtypes = [PtrClient]
-
+PtrClientContext = c_void_p
 PtrRpcResult = c_void_p
 PtrServerContext = c_void_p
+
 RPC = CFUNCTYPE(None, PtrServerContext, c_char_p)
 
-library.swiboe_new_rpc.restype = None
-library.swiboe_new_rpc.argtypes = [PtrClient, c_char_p, c_uint16, RPC]
+def load_shared_library(shared_library):
+    # TODO(sirver): This needs extending for Windows.
+    if shared_library is None:
+        shared_library = os.getenv('SWIBOE_LIBRARY')
 
-library.swiboe_rpc_ok.restype = PtrRpcResult
-library.swiboe_rpc_ok.argtypes = [c_char_p]
+    if shared_library is None:
+        if platform.system() == 'Darwin':
+            shared_library = 'libswiboe.dylib'
+        else:
+            shared_library = 'libswiboe.so'
+    library = ctypes.cdll.LoadLibrary(shared_library)
 
-library.swiboe_rpc_not_handled.restype = PtrRpcResult
-library.swiboe_rpc_not_handled.argtypes = []
+    library.swiboe_connect.restype = PtrClient
+    library.swiboe_connect.argtypes = [c_char_p]
 
-library.swiboe_rpc_error.restype = PtrRpcResult
-library.swiboe_rpc_error.argtypes = [c_char_p, c_char_p]
+    library.swiboe_disconnect.restype = None
+    library.swiboe_disconnect.argtypes = [PtrClient]
 
-PtrClientContext = c_void_p
-library.swiboe_client_call_rpc.restype = PtrClientContext
-library.swiboe_client_call_rpc.argtypes = [PtrClient, c_char_p, c_char_p]
+    library.swiboe_new_rpc.restype = None
+    library.swiboe_new_rpc.argtypes = [PtrClient, c_char_p, c_uint16, RPC]
 
-library.swiboe_client_context_wait.restype = PtrRpcResult
-library.swiboe_client_context_wait.argtypes = [PtrClientContext]
+    library.swiboe_rpc_ok.restype = PtrRpcResult
+    library.swiboe_rpc_ok.argtypes = [c_char_p]
 
-library.swiboe_server_context_finish.restype = None
-library.swiboe_server_context_finish.argtypes = [PtrServerContext, PtrRpcResult]
+    library.swiboe_rpc_not_handled.restype = PtrRpcResult
+    library.swiboe_rpc_not_handled.argtypes = []
 
-library.swiboe_server_context_call_rpc.restype = PtrClientContext
-library.swiboe_server_context_call_rpc.argtypes = [PtrServerContext, c_char_p, c_char_p]
+    library.swiboe_rpc_error.restype = PtrRpcResult
+    library.swiboe_rpc_error.argtypes = [c_char_p, c_char_p]
 
-library.swiboe_rpc_result_is_ok.restype = bool
-library.swiboe_rpc_result_is_ok.argtypes = [PtrRpcResult]
+    library.swiboe_client_call_rpc.restype = PtrClientContext
+    library.swiboe_client_call_rpc.argtypes = [PtrClient, c_char_p, c_char_p]
 
-library.swiboe_rpc_result_unwrap.restype = c_char_p
-library.swiboe_rpc_result_unwrap.argtypes = [PtrRpcResult]
+    library.swiboe_client_context_wait.restype = PtrRpcResult
+    library.swiboe_client_context_wait.argtypes = [PtrClientContext]
 
-library.swiboe_client_context_recv.restype = c_char_p
-library.swiboe_client_context_recv.argtypes = [PtrClientContext]
+    library.swiboe_server_context_finish.restype = None
+    library.swiboe_server_context_finish.argtypes = [
+        PtrServerContext, PtrRpcResult]
 
-library.swiboe_server_context_update.restype = None
-library.swiboe_server_context_update.argtypes = [PtrServerContext, c_char_p]
+    library.swiboe_server_context_call_rpc.restype = PtrClientContext
+    library.swiboe_server_context_call_rpc.argtypes = [
+        PtrServerContext, c_char_p, c_char_p]
+
+    library.swiboe_rpc_result_is_ok.restype = bool
+    library.swiboe_rpc_result_is_ok.argtypes = [PtrRpcResult]
+
+    library.swiboe_rpc_result_unwrap.restype = c_char_p
+    library.swiboe_rpc_result_unwrap.argtypes = [PtrRpcResult]
+
+    library.swiboe_client_context_recv.restype = c_char_p
+    library.swiboe_client_context_recv.argtypes = [PtrClientContext]
+
+    library.swiboe_server_context_update.restype = None
+    library.swiboe_server_context_update.argtypes = [PtrServerContext, c_char_p]
+
+    return library
+
+
+class SwiboeLibrary(object):
+    LIBRARY = None
+    LIBRARY_LOADING_ARGUMENT = None
+
+    def __init__(self, shared_library=None):
+        if SwiboeLibrary.LIBRARY is None:
+            SwiboeLibrary.LIBRARY = load_shared_library(shared_library)
+            SwiboeLibrary.LIBRARY_LOADING_ARGUMENT = shared_library
+        elif SwiboeLibrary.LIBRARY_LOADING_ARGUMENT != shared_library:
+            raise RuntimeError(
+                'SwiboeLibrary() initialized with different arguments (was: %r, is: %r)' %
+                (SwiboeLibrary.LIBRARY_LOADING_ARGUMENT, shared_library))
+
+    def __getattribute__(self, name):
+        # Using __getattr__ actually crashes here.
+        return SwiboeLibrary.LIBRARY.__getattribute__(name)
--- c_client/test.py
+++ c_client/test.py
@@ -4,99 +4,98 @@
 # Licensed under the Apache License, Version 2.0. See LICENSE.txt
 # in the project root for license information.
 
+import argparse
 import json
 import os
 import subprocess
+import sys
 import time
 import unittest
 
-from swiboe import library
+from swiboe import SwiboeLibrary
 import swiboe
 
-# NOCOM(#sirver): make SWIBOE_LIBRARY passable on the commandline for the test runner.
-
-# NOCOM(#sirver): should not be hardcoded.
-TEST_SERVER="../target/debug/test_server"
-
 class TestSwiboeClientLowLevel(unittest.TestCase):
+    SHARED_LIBRARY = None
+    TEST_SERVER = None
 
     def setUp(self):
         self.server_process = subprocess.Popen(
-                [TEST_SERVER], stdout = subprocess.PIPE)
+                [self.TEST_SERVER], stdout = subprocess.PIPE)
         self.test_dir = self.server_process.stdout.readline().strip()
         # NOCOM(#sirver): this should just be in the json file.
         self.socket_name = os.path.join(self.test_dir, "_socket")
 
+        self.library = SwiboeLibrary(self.SHARED_LIBRARY)
+
     def tearDown(self):
         self.server_process.kill()
 
-    # NOCOM(#sirver): all of these tests should bring up the swiboe server
-    # themselves, so that we can run tests in isolation and parallel.
     def test_connect_and_disconnect(self):
-        client = library.swiboe_connect(self.socket_name)
-        library.swiboe_disconnect(client)
+        client = self.library.swiboe_connect(self.socket_name)
+        self.library.swiboe_disconnect(client)
 
     def test_new_rpc(self):
-        client = library.swiboe_connect(self.socket_name)
+        client = self.library.swiboe_connect(self.socket_name)
 
         def callback(server_context, args_string):
             return
         rpc_callback = swiboe.RPC(callback)
-        library.swiboe_new_rpc(client, "list_rust_files", 100, rpc_callback)
-        library.swiboe_disconnect(client)
+        self.library.swiboe_new_rpc(client, "list_rust_files", 100, rpc_callback)
+        self.library.swiboe_disconnect(client)
 
     def test_call_successfull_rpc(self):
-        serving_client = library.swiboe_connect(self.socket_name)
+        serving_client = self.library.swiboe_connect(self.socket_name)
         golden_return = { "blub": "foo" }
         def callback(server_context, args_string):
-            call_result = library.swiboe_rpc_ok(json.dumps(golden_return))
-            library.swiboe_server_context_finish(server_context, call_result)
+            call_result = self.library.swiboe_rpc_ok(json.dumps(golden_return))
+            self.library.swiboe_server_context_finish(server_context, call_result)
         rpc_callback = swiboe.RPC(callback)
-        library.swiboe_new_rpc(serving_client, "test.test", 100, rpc_callback)
+        self.library.swiboe_new_rpc(serving_client, "test.test", 100, rpc_callback)
 
-        client = library.swiboe_connect(self.socket_name)
-        client_context = library.swiboe_client_call_rpc(
+        client = self.library.swiboe_connect(self.socket_name)
+        client_context = self.library.swiboe_client_call_rpc(
                 client, "test.test", "null")
-        call_result = library.swiboe_client_context_wait(client_context)
-        self.assertTrue(library.swiboe_rpc_result_is_ok(call_result));
+        call_result = self.library.swiboe_client_context_wait(client_context)
+        self.assertTrue(self.library.swiboe_rpc_result_is_ok(call_result));
 
-        json_blob = library.swiboe_rpc_result_unwrap(call_result)
+        json_blob = self.library.swiboe_rpc_result_unwrap(call_result)
         print "#sirver json_blob: %r" % (json_blob)
         self.assertEqual(golden_return, json.loads(json_blob))
 
-        library.swiboe_disconnect(client)
-        library.swiboe_disconnect(serving_client)
+        self.library.swiboe_disconnect(client)
+        self.library.swiboe_disconnect(serving_client)
 
 
 
 
 # NOCOM(#sirver): delete main function.
-def main():
-    client = library.swiboe_connect("/tmp/blub.socket")
+# def main():
+    # client = library.swiboe_connect("/tmp/blub.socket")
 
-    def callback(server_context, args_string):
-        args = json.loads(args_string)
-        print("callback called %s" % args)
-        directory = args['directory']
+    # def callback(server_context, args_string):
+        # args = json.loads(args_string)
+        # print("callback called %s" % args)
+        # directory = args['directory']
 
-        client_context = library.swiboe_client_call_rpc(client, "list_files", json.dumps({
-            "directory": directory,
-        }))
+        # client_context = library.swiboe_client_call_rpc(client, "list_files", json.dumps({
+            # "directory": directory,
+        # }))
 
-        # NOCOM(#sirver): look into returning errors from RPCs, not only successes.
-        while True:
-            json_blob = library.swiboe_client_context_recv(client_context)
-            if json_blob is None:
-                break
-            value = json.loads(json_blob)
-            value['files'] = [ v for v in value['files'] if
-                    v.endswith(".rs") or v.endswith(".toml") ]
-            value_str = json.dumps(value)
-            print "#sirver value_str: %r" % (value_str)
-            library.swiboe_server_context_update(server_context, value_str)
+        # # NOCOM(#sirver): look into returning errors from RPCs, not only successes.
+        # while True:
+            # json_blob = library.swiboe_client_context_recv(client_context)
+            # if json_blob is None:
+                # break
+            # value = json.loads(json_blob)
+            # value['files'] = [ v for v in value['files'] if
+                    # v.endswith(".rs") or v.endswith(".toml") ]
+            # value_str = json.dumps(value)
+            # print "#sirver value_str: %r" % (value_str)
+            # library.swiboe_server_context_update(server_context, value_str)
 
-        call_result = library.swiboe_client_context_wait(client_context)
-        library.swiboe_server_context_finish(server_context, call_result)
+        # call_result = library.swiboe_client_context_wait(client_context)
+        # library.swiboe_server_context_finish(server_context, call_result)
 
 
         # result = library.swiboe_rpc_ok("{}")
@@ -110,15 +109,55 @@ def main():
         # library.swiboe_server_context_finish(server_context, result)
         # # server_context is no longer valid.
 
-    rpc_callback = swiboe.RPC(callback)
+    # rpc_callback = swiboe.RPC(callback)
 
-    # TODO(sirver): The client should complain if the same RPC is registered twice.
-    library.swiboe_new_rpc(client, "list_rust_files", 100, rpc_callback)
+    # # TODO(sirver): The client should complain if the same RPC is registered twice.
+    # library.swiboe_new_rpc(client, "list_rust_files", 100, rpc_callback)
 
-    while 1:
-        time.sleep(1)
+    # while 1:
+        # time.sleep(1)
 
-    library.swiboe_disconnect(client)
+    # library.swiboe_disconnect(client)
+
+def flatten_test_suite(suite):
+        flatten = unittest.TestSuite()
+        for test in suite:
+            if isinstance(test, unittest.TestSuite):
+                flatten.addTests(flatten_test_suite(test))
+            else:
+                flatten.addTest(test)
+        return flatten
+
+def parse_args():
+    p = argparse.ArgumentParser(description=
+        "Test runner for the python interface. Also tests the full C ABI."
+    )
+
+    p.add_argument("--shared_library", type=str, default=None, help = "The shared library that will be loaded by ctypes for testing.")
+    p.add_argument("--test_server", type=str, default="../target/debug/test_server", help=
+            "The test server binary.")
+    p.add_argument('-v', '--verbose', action='store_true', default=False,
+                     help='print name of tests as they are executed')
+
+    return p.parse_args()
+
+def main():
+    args = parse_args()
+
+    all_test_suites = unittest.defaultTestLoader.discover(start_dir='.')
+    suite = unittest.TestSuite()
+
+    tests = set()
+    for test in flatten_test_suite(all_test_suites):
+        test.SHARED_LIBRARY = args.shared_library
+        test.TEST_SERVER = args.test_server
+        tests.add(test)
+    suite.addTests(tests)
+    # TODO(sirver): Add -v flag.
+    successful = unittest.TextTestRunner(verbosity=0 if not args.verbose else 3,
+                                          failfast=False).run(suite).wasSuccessful()
+    return 0 if successful else 1
+
 
 if __name__ == '__main__':
-    unittest.main()
+    sys.exit(main())
